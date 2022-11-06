6ea16aa982d52e2683e0f22fe564e2b2875a66a4
TylerLucas <tjlucas519@outlook.com>
2022-11-05 19:14:09 -0500

    Player rotates towards direction of movement, useing cinemachine for Camera, changed scene slightly, have slight jump buffer.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,14 @@ public class PlayerController : MonoBehaviour
 {
 	private Rigidbody playerRB;
 	private bool isOnGround = true;
+	private float? lastGroundedTime;
+	private float? jumpButtonPressedTime;
 
 	public float speed;
 	public float jumpForce;
 	public float fallMuliplier;
+	public float rotationSpeed;
+	public float jumpGracePeriod;
 
 	void Start()
 	{
@@ -17,17 +21,40 @@ public class PlayerController : MonoBehaviour
 	}
 	void Update()
 	{
-		float movementX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-		transform.position += new Vector3(movementX, 0, 0);
-		
-		if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+		//Movement
+		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0) * speed * Time.deltaTime;
+		transform.Translate(movement, Space.World);
+
+		//Jump Buffer
+		if (isOnGround)
 		{
-			playerRB.velocity += Vector3.up * jumpForce;
-			isOnGround = false;
+			lastGroundedTime = Time.time;
+		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			jumpButtonPressedTime = Time.time;
+		}
+		//Jump
+		if (Time.time - lastGroundedTime <= jumpGracePeriod)
+		{
+			if (Time.time - jumpButtonPressedTime <= jumpGracePeriod)
+			{
+				playerRB.velocity += Vector3.up * jumpForce;
+				isOnGround = false;
+				jumpButtonPressedTime = null;
+				lastGroundedTime = null;
+			}
 		}
 		if (playerRB.velocity.y < 0)
 		{
 			playerRB.velocity += (fallMuliplier - 1) * Physics.gravity.y * Time.deltaTime * Vector3.up;
+		}
+
+		//When Moving Character Rotation
+		if (movement != Vector3.zero)
+		{
+			Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
 		}
 	}
 	private void OnCollisionEnter(Collision collision)
